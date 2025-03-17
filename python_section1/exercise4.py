e1563ffef524d4e58db00f94f56a69be6b795b27
Faraz Fayyaz <farazfayyazwork@gmail.com>
2025-03-17 15:52:18 -0400

    exercise 4 complete

---
+++ python_section1/exercise4.py
@@ -0,0 +1,11 @@
+# Write code below 💖
+
+# Fun fact: I have a 4.0 GPA
+
+print('FFFF    F')
+print('F   F   F')
+print('F   F   F')
+print('F   F   F')
+print('F   F   F')
+print('F   F   F')
+print('FFFF    FFFFF')
