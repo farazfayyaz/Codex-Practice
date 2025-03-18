560d1e83a91ab9a38f50cd32f0e92291549973ee
Faraz Fayyaz <farazfayyazwork@gmail.com>
2025-03-18 15:43:18 -0400

    exercise 3 complete

---
+++ python_section1/exercise3.py
@@ -0,0 +1,11 @@
+
+# Python is run one line at a time, from top to bottom.
+
+# We can output multiple messages by using multiple print() functions.
+
+# Write code below 💖
+
+print('       1')
+print('     2 3')
+print('   4 5 6')
+print('7 8 9 10')
