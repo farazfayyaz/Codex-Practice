f7b95b8d985aeb499b0e6fe3c4e4b9c5a9be00bb
Faraz Fayyaz <farazfayyazwork@gmail.com>
2025-05-27 18:26:31 -0400

    complete section 2 ex 11

---
+++ python_section2/exercise11/main.py
@@ -0,0 +1,7 @@
+# pure functions - output derives from its input values
+# impure - has side effect
+
+def calculate_circle_area(radius):
+    return 3.14 * radius**2
+
+print(calculate_circle_area(12))
